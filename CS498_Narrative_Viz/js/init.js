766fe35bd8bc86c02d6113a255ba575186426690
jazowsk2 <68122284+jazowsk2@users.noreply.github.com>
2020-07-11 13:36:24 -0700

    Working plot

--- CS498_Narrative_Viz/js/init.js
+++ CS498_Narrative_Viz/js/init.js
@@ -1,4 +1,18 @@
 async function init() {
+
+	// set the dimensions and margins of the graph
+	var margin = {top: 10, right: 50, bottom: 50, left: 50},
+		width = 800 - margin.left - margin.right,
+		height = 700 - margin.top - margin.bottom;
+
+	// append the svg object to the body of the page
+	var svg = d3.select("svg")
+		.attr("width", width + margin.left + margin.right)
+		.attr("height", height + margin.top + margin.bottom)
+		.append("g")
+		.attr("transform","translate(" + margin.left + "," + margin.top + ")");
+		  
+		  
 	var data_index = [];
 	const data = await d3.csv(
 	'https://jazowsk2.github.io/CS498_Narrative_Viz/data/LA_County_Covid19_cases_deaths_date_table.csv');
@@ -6,6 +20,7 @@ async function init() {
 	// convert strings to int
 	data.forEach(function(d) {
 			d.index = +d.index;
+			d.date_use = d3.timeParse("%m/%d/%Y")(d.date_use);
 			d.total_cases = +d.total_cases;
 			d.new_case = +d.new_case;
 			d.avg_cases = +d.avg_cases;
@@ -14,21 +29,24 @@ async function init() {
 			d.avg_deaths = +d.avg_deaths;
 			data_index = data_index.concat(d.index);
 		});	
-	var plot_width = 600;
-	var plot_height = 500;
-	var case_max = Math.max.apply(Math,data.map(function(o) {return o.new_case}));
-	var xs = d3.scaleBand().domain(data_index).range([0,plot_width]);
-	var ys = d3.scaleLinear().domain([0,case_max]).range([plot_height,0]);
-
-	d3.select("svg").append("g").attr("transform","translate(50,50)")
-	.selectAll("rect").data(data).enter().append("rect")
-	.attr("x",function (d,i) {return xs(d.index);}).attr("height",function(d,i) { return plot_height - ys(d.new_case);})
-	.attr("width",xs.bandwidth()).attr("y",function(d,i) { return ys(d.new_case);});
-	d3.select("svg").append("g").attr("transform","translate(50,50)").call(d3.axisLeft(ys).tickValues([0,1000,2000,case_max]).tickFormat(d3.format("~s")));
-	d3.select("svg").append("g").attr("transform","translate(50,550)").call(d3.axisBottom(xs).tickValues([0,40,80,120]).tickFormat(d3.format("~s")));
 	
-	// Add the line
-    d3.select("svg").append("path").attr("transform","translate(50,50)")
+	// add the bar graph
+	var case_max = Math.max.apply(Math,data.map(function(o) {return o.new_case}));
+	var xs = d3.scaleBand().domain(data_index).range([0,width]);
+	var ys = d3.scaleLinear().domain([0,case_max]).range([height,0]);
+
+	svg.append("g")
+		.selectAll("rect").data(data).enter().append("rect")
+		.attr("x",function (d,i) {return xs(d.index);}).attr("height",function(d,i) { return height - ys(d.new_case);})
+		.attr("width",xs.bandwidth()).attr("y",function(d,i) { return ys(d.new_case);});
+	
+	// Add cases axis
+	svg.append("g")
+		.call(d3.axisLeft(ys).tickValues([0,1000,2000,case_max]).tickFormat(d3.format("~s")));
+	//svg.append("g").attr("transform","translate(50,550)").call(d3.axisBottom(xs));//.tickValues([0,40,80,120]).tickFormat(d3.format("~s")));
+	  
+	// Add the cases line
+    svg.append("path")
       .datum(data)
       .attr("fill", "none")
       .attr("stroke", "black")
@@ -36,5 +54,34 @@ async function init() {
       .attr("d", d3.line()
         .x(function(d) { return xs(d.index) })
         .y(function(d) { return ys(d.avg_cases) })
-        )
+        );
+
+	// Add X axis --> it is a date format
+    var x = d3.scaleTime()
+      .domain(d3.extent(data, function(d) { return d.date_use; }))
+      .range([ 0, width ]);
+    svg.append("g")
+      .attr("transform","translate(0,"+height+")")
+      .call(d3.axisBottom(x));
+	  
+	  
+	// Add the deaths line
+	var death_max = Math.max.apply(Math,data.map(function(o) {return o.avg_deaths}));
+	var ys2 = d3.scaleLinear().domain([0,death_max]).range([height,0]);
+    svg.append("path")
+      .datum(data)
+      .attr("fill", "none")
+      .attr("stroke", "green")
+      .attr("stroke-width", 1.5)
+      .attr("d", d3.line()
+        .x(function(d) { return xs(d.index) })
+        .y(function(d) { return ys2(d.avg_deaths) })
+        );	
+		
+	// add deaths axis
+	svg.append("g").attr("transform","translate("+width+",0)")
+		.call(d3.axisRight(ys2).tickValues([0,15,30,death_max]).tickFormat(d3.format("~s")));
+		
+	// add a legend
+	
 }
